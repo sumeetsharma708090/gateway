60f2d4cb47599c65f20cc104da90fbe4c883caf4
Sumeet Sharma <sumeetsharma@imsspl.com>
2023-10-11 09:55:19 +0530

    commit

---
+++ api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import co.elastic.apm.attach.ElasticApmAttacher;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
